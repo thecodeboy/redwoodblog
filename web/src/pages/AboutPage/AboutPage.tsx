e6ed01a8775096dbdb6d1e81de072a36d7a1d599
Aishik Saha <10260999+thecodeboy@users.noreply.github.com>
2024-03-09 17:09:52 +0530

    added HomePage, AboutPage, BlogLayout and some CSS

---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,14 @@
+import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <Metadata title="About" description="About page" />
+
+      <p>This site was created to demonstrate my mastery of Redwood.</p>
+    </>
+  )
+}
+
+export default AboutPage
---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,14 @@
+import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <Metadata title="Home" description="Home page" />
+
+      <main>Home</main>
+    </>
+  )
+}
+
+export default HomePage
